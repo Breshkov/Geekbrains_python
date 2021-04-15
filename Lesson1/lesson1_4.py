5e0e3c81e53341f0bc0d662efee61aa45a71e6be
Breshkov <breshkov@list.ru>
2021-04-15 19:14:05 +0300

    test

---
+++ Lesson1/lesson1_4.py
@@ -0,0 +1,8 @@
+i =int(input())
+r = -1
+while i > 10:
+    d = i % 10
+    i //= 10
+    if d > r:
+        r = d
+print(r)
---
+++ Lesson1/lesson1_6.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+i=1
+while a<b:
+    i=i+1
+    a=a*1.1
+print('Ha ',i, '-й день спортсмен пробежит больше ', b,' km' )
